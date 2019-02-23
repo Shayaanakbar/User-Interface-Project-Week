430b8d366cd58c6a5df521747132a98886cbdefe
shayaanakbar <shayaanakbar1106@gmail.com>
2019-02-23 09:15:54 -0600

    Working on JS Nav bar responsive to an extent cannot tab over pages

--- js/index.js
+++ js/index.js
@@ -10,3 +10,60 @@ function openNav() {
     document.querySelector(".menu-close").style.display = "none";
     document.querySelector(".menu-expand").style.display = "block";
   }
+
+
+  for(let i =0; i < hamButton.length; i++) {
+    hamButton[i].addEventListener("click", showNav);
+
+} 
+
+
+
+  class TabLink {
+    constructor(element) {
+      this.element = element;
+      this.data = this.element.dataset.tab;
+      this.itemElement = document.querySelector(`.tabs-item[data-tab= '${this.data}']`);
+      this.tabItem = new TabItem(this.itemElement);
+      this.element.addEventListener("click", () => { this.select(); });
+  };
+
+  select() {
+    
+    const links = document.querySelectorAll(".tabs-link");
+
+    links.forEach( link => {
+      link.classList.remove('tabs-link-selected')
+    });
+
+    this.element.classList.add("tabs-link-selected")
+
+    this.tabItem.select()
+
+
+  }
+  };
+  class tabItem {
+    constructor(element) {
+      this.element = element;
+    }
+
+    select() {
+
+
+      const items = document.querySelectorAll(".tabs-item");
+
+      items.forEach( item => {
+        item.classList.remove('tabs-item-selected');
+      })
+
+
+      this.element.classList.add('tabs-item-selected');
+    }
+  }
+
+
+  links = document.querySelectorAll(".tabs-link");
+  links.forEach(function (link) {
+    return new TabLink(link)
+  });
